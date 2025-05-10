9cd9e293de4ff7caa269d4ef9c1d6b12f42c2c24
Ростислав Ермолаев <0988560505r@gmail.com>
2025-05-10 23:14:22 +0300

    good)

--- src/app/components/NoAutorazeMenu.js
+++ src/app/components/NoAutorazeMenu.js
@@ -730,6 +730,262 @@
 
 
 
+// "use client";
+// import React, { useState, useRef, useEffect } from "react";
+// import Logo from "./Logo";
+// import { useLanguage } from "@/app/LanguageContext";
+// import {
+//   Drawer,
+//   List,
+//   ListItem,
+//   ListItemText,
+//   IconButton,
+//   Collapse,
+//   MenuItem,
+//   Typography,
+//   Box,
+//   Modal,
+//   Snackbar,
+//   Alert,
+// } from "@mui/material";
+// import { Close, ArrowDropDown } from "@mui/icons-material";
+// import CreateUser from "./CreateUser";
+// import styles from "@/app/styles/Menu.styles.module.scss";
+
+// const translations = {
+//   ua: {
+//     slogan: "Оренда житла по всій Україні без посередників.",
+//     language: "Мова",
+//     currency: "Валюта",
+//     login: "Увійти/Зареєструватися",
+//     housingSearch: "Пошук житла",
+//     rentalConditions: "Умови оренди",
+//     faq: "Часті питання",
+//     blog: "Блог",
+//     contact: "Зв'язатися з підтримкою",
+//     listYourProperty: "Здати своє помешкання",
+//     registerMessage: "Щоб розмістити свій об’єкт, потрібно зареєструватися",
+//   },
+//   ru: {
+//     slogan: "Аренда жилья по всей Украине без посредников.",
+//     language: "Язык",
+//     currency: "Валюта",
+//     login: "Войти/Зарегистрироваться",
+//     housingSearch: "Поиск жилья",
+//     rentalConditions: "Условия аренды",
+//     faq: "Часто задаваемые вопросы",
+//     blog: "Блог",
+//     contact: "Связаться с поддержкой",
+//     listYourProperty: "Сдать свое жилье",
+//     registerMessage: "Чтобы разместить свой объект, нужно зарегистрироваться",
+//   },
+// };
+
+// const NoAutorazeMenu = () => {
+//   const [isMenuOpen, setIsMenuOpen] = useState(true);
+//   const [isLanguageMenuOpen, setIsLanguageMenuOpen] = useState(false);
+//   const [isCurrencyMenuOpen, setIsCurrencyMenuOpen] = useState(false);
+//   const [isCreateUserOpen, setIsCreateUserOpen] = useState(false);
+//   const [isAlertOpen, setIsAlertOpen] = useState(false);
+//   const menuRef = useRef(null);
+//   const { currentLanguage, onLanguageToggle } = useLanguage();
+//   const t = translations[currentLanguage];
+
+//   const handleCloseMenu = () => setIsMenuOpen(false);
+
+//   const handleCreateUserOpen = () => {
+//     setIsAlertOpen(true);
+//     setIsCreateUserOpen(true);
+//     handleCloseMenu();
+//   };
+
+//   const handleCreateUserClose = () => setIsCreateUserOpen(false);
+
+//   const handleAlertClose = (event, reason) => {
+//     if (reason === "clickaway") return;
+//     setIsAlertOpen(false);
+//   };
+
+//   const handleClickOutside = (event) => {
+//     if (menuRef.current && !menuRef.current.contains(event.target)) {
+//       setIsMenuOpen(false);
+//       setIsLanguageMenuOpen(false);
+//       setIsCurrencyMenuOpen(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (isMenuOpen) {
+//       document.addEventListener("mousedown", handleClickOutside);
+//     }
+//     return () => {
+//       document.removeEventListener("mousedown", handleClickOutside);
+//     };
+//   }, [isMenuOpen]);
+
+//   return (
+//     <>
+//       <Drawer anchor="right" open={isMenuOpen} onClose={handleCloseMenu}>
+//         <Box sx={{ width: 300 }} role="presentation" ref={menuRef}>
+//           <Box
+//             className={styles.menuHeader}
+//             sx={{
+//               display: "flex",
+//               alignItems: "center",
+//               justifyContent: "space-between",
+//               p: 2,
+//             }}
+//           >
+//             <Typography variant="h6" className={styles.name}>
+//               <Logo />
+//             </Typography>
+//             <IconButton onClick={handleCloseMenu}>
+//               <Close />
+//             </IconButton>
+//           </Box>
+
+//           <Typography variant="body1" sx={{ p: 2, color: "blue" }}>
+//             {t.slogan}
+//           </Typography>
+
+//           <List>
+//             <ListItem button onClick={handleCreateUserOpen}>
+//               <ListItemText
+//                 primary={<span style={{ color: "blue" }}>{t.login}</span>}
+//               />
+//             </ListItem>
+
+//             <ListItem
+//               button
+//               onClick={() => setIsLanguageMenuOpen(!isLanguageMenuOpen)}
+//             >
+//               <ListItemText
+//                 primary={<span style={{ color: "blue" }}>{t.language}</span>}
+//               />
+//               <ArrowDropDown />
+//             </ListItem>
+//             <Collapse in={isLanguageMenuOpen}>
+//               <List>
+//                 <MenuItem onClick={() => onLanguageToggle("ua")}>UA</MenuItem>
+//                 <MenuItem onClick={() => onLanguageToggle("ru")}>RU</MenuItem>
+//               </List>
+//             </Collapse>
+
+//             <ListItem
+//               button
+//               onClick={() => setIsCurrencyMenuOpen(!isCurrencyMenuOpen)}
+//             >
+//               <ListItemText
+//                 primary={<span style={{ color: "blue" }}>{t.currency}</span>}
+//               />
+//               <ArrowDropDown />
+//             </ListItem>
+//             <Collapse in={isCurrencyMenuOpen}>
+//               <List>
+//                 <MenuItem>USD</MenuItem>
+//                 <MenuItem>EUR</MenuItem>
+//                 <MenuItem>UAH</MenuItem>
+//               </List>
+//             </Collapse>
+
+//             <ListItem button onClick={handleCreateUserOpen}>
+//               <Box
+//                 sx={{
+//                   width: "100%",
+//                   textAlign: "center",
+//                   fontSize: "18px",
+//                   fontWeight: 600,
+//                   color: "#1e88e5",
+//                   fontFamily: "Roboto, sans-serif",
+//                   boxShadow: "0px 2px 6px rgba(0, 0, 0, 0.1)",
+//                   borderRadius: "8px",
+//                   padding: "10px 16px",
+//                   backgroundColor: "#ffffff",
+//                   transition: "all 0.3s ease",
+//                   "&:hover": {
+//                     boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.2)",
+//                     backgroundColor: "#f5f5f5",
+//                   },
+//                 }}
+//               >
+//                 {t.listYourProperty}
+//               </Box>
+//             </ListItem>
+
+//             {[
+//               t.housingSearch,
+//               t.rentalConditions,
+//               t.faq,
+//               t.blog,
+//               t.contact,
+//             ].map((text) => (
+//               <ListItem button key={text}>
+//                 <ListItemText
+//                   primary={<span style={{ color: "blue" }}>{text}</span>}
+//                 />
+//               </ListItem>
+//             ))}
+//           </List>
+//         </Box>
+//       </Drawer>
+
+//       <Snackbar
+//         open={isAlertOpen}
+//         autoHideDuration={7000}
+//         onClose={handleAlertClose}
+//         anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+//         sx={{
+//           mt: '10vh',
+//           zIndex: 1400,
+//         }}
+//         action={
+//           <IconButton
+//             size="small"
+//             aria-label="close"
+//             color="inherit"
+//             onClick={handleAlertClose}
+//           >
+//             <Close fontSize="small" />
+//           </IconButton>
+//         }
+//       >
+//         <Alert onClose={handleAlertClose} severity="info" sx={{ width: "100%" }}>
+//           {t.registerMessage}
+//         </Alert>
+//       </Snackbar>
+
+//       <Modal
+//         open={isCreateUserOpen}
+//         onClose={handleCreateUserClose}
+//         aria-labelledby="create-user-modal"
+//         aria-describedby="create-user-modal-description"
+//       >
+//         <Box
+//           sx={{
+//             position: "absolute",
+//             top: "50%",
+//             left: "50%",
+//             transform: "translate(-50%, -50%)",
+//             width: 400,
+//             bgcolor: "background.paper",
+//             boxShadow: 24,
+//             p: 4,
+//             borderRadius: 2,
+//           }}
+//         >
+//           <CreateUser onClose={handleCreateUserClose} />
+//         </Box>
+//       </Modal>
+//     </>
+//   );
+// };
+
+// export default NoAutorazeMenu;
+
+
+
+
+
 "use client";
 import React, { useState, useRef, useEffect } from "react";
 import Logo from "./Logo";
@@ -744,9 +1000,12 @@ import {
   MenuItem,
   Typography,
   Box,
-  Modal,
   Snackbar,
   Alert,
+  Dialog,
+  DialogContent,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
 import { Close, ArrowDropDown } from "@mui/icons-material";
 import CreateUser from "./CreateUser";
@@ -791,6 +1050,9 @@ const NoAutorazeMenu = () => {
   const { currentLanguage, onLanguageToggle } = useLanguage();
   const t = translations[currentLanguage];
 
+  const theme = useTheme();
+  const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
+
   const handleCloseMenu = () => setIsMenuOpen(false);
 
   const handleCreateUserOpen = () => {
@@ -801,13 +1063,13 @@ const NoAutorazeMenu = () => {
 
   const handleCreateUserClose = () => setIsCreateUserOpen(false);
 
-  const handleAlertClose = (event, reason) => {
-    if (reason === "clickaway") return;
+  const handleAlertClose = (e, reason) => {
+    if (reason === 'clickaway') return;
     setIsAlertOpen(false);
   };
 
-  const handleClickOutside = (event) => {
-    if (menuRef.current && !menuRef.current.contains(event.target)) {
+  const handleClickOutside = e => {
+    if (menuRef.current && !menuRef.current.contains(e.target)) {
       setIsMenuOpen(false);
       setIsLanguageMenuOpen(false);
       setIsCurrencyMenuOpen(false);
@@ -815,69 +1077,39 @@ const NoAutorazeMenu = () => {
   };
 
   useEffect(() => {
-    if (isMenuOpen) {
-      document.addEventListener("mousedown", handleClickOutside);
-    }
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
+    if (isMenuOpen) document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
   }, [isMenuOpen]);
 
   return (
     <>
       <Drawer anchor="right" open={isMenuOpen} onClose={handleCloseMenu}>
-        <Box sx={{ width: 300 }} role="presentation" ref={menuRef}>
-          <Box
-            className={styles.menuHeader}
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-              p: 2,
-            }}
-          >
-            <Typography variant="h6" className={styles.name}>
-              <Logo />
-            </Typography>
-            <IconButton onClick={handleCloseMenu}>
-              <Close />
-            </IconButton>
+        <Box sx={{ width: 300 }} ref={menuRef}>
+          <Box className={styles.menuHeader} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', p: 2 }}>
+            <Logo />
+            <IconButton onClick={handleCloseMenu}><Close /></IconButton>
           </Box>
 
-          <Typography variant="body1" sx={{ p: 2, color: "blue" }}>
-            {t.slogan}
-          </Typography>
+          <Typography sx={{ p: 2, color: 'blue' }}>{t.slogan}</Typography>
 
           <List>
             <ListItem button onClick={handleCreateUserOpen}>
-              <ListItemText
-                primary={<span style={{ color: "blue" }}>{t.login}</span>}
-              />
+              <ListItemText primary={<Typography color="primary">{t.login}</Typography>} />
             </ListItem>
 
-            <ListItem
-              button
-              onClick={() => setIsLanguageMenuOpen(!isLanguageMenuOpen)}
-            >
-              <ListItemText
-                primary={<span style={{ color: "blue" }}>{t.language}</span>}
-              />
+            <ListItem button onClick={() => setIsLanguageMenuOpen(!isLanguageMenuOpen)}>
+              <ListItemText primary={<Typography color="primary">{t.language}</Typography>} />
               <ArrowDropDown />
             </ListItem>
             <Collapse in={isLanguageMenuOpen}>
               <List>
-                <MenuItem onClick={() => onLanguageToggle("ua")}>UA</MenuItem>
-                <MenuItem onClick={() => onLanguageToggle("ru")}>RU</MenuItem>
+                <MenuItem onClick={() => onLanguageToggle('ua')}>UA</MenuItem>
+                <MenuItem onClick={() => onLanguageToggle('ru')}>RU</MenuItem>
               </List>
             </Collapse>
 
-            <ListItem
-              button
-              onClick={() => setIsCurrencyMenuOpen(!isCurrencyMenuOpen)}
-            >
-              <ListItemText
-                primary={<span style={{ color: "blue" }}>{t.currency}</span>}
-              />
+            <ListItem button onClick={() => setIsCurrencyMenuOpen(!isCurrencyMenuOpen)}>
+              <ListItemText primary={<Typography color="primary">{t.currency}</Typography>} />
               <ArrowDropDown />
             </ListItem>
             <Collapse in={isCurrencyMenuOpen}>
@@ -889,25 +1121,7 @@ const NoAutorazeMenu = () => {
             </Collapse>
 
             <ListItem button onClick={handleCreateUserOpen}>
-              <Box
-                sx={{
-                  width: "100%",
-                  textAlign: "center",
-                  fontSize: "18px",
-                  fontWeight: 600,
-                  color: "#1e88e5",
-                  fontFamily: "Roboto, sans-serif",
-                  boxShadow: "0px 2px 6px rgba(0, 0, 0, 0.1)",
-                  borderRadius: "8px",
-                  padding: "10px 16px",
-                  backgroundColor: "#ffffff",
-                  transition: "all 0.3s ease",
-                  "&:hover": {
-                    boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.2)",
-                    backgroundColor: "#f5f5f5",
-                  },
-                }}
-              >
+              <Box sx={{ width: '100%', textAlign: 'center', typography: 'h6', color: 'primary.main', p: 1, boxShadow: 1, borderRadius: 1, '&:hover': { boxShadow: 3 } }}>
                 {t.listYourProperty}
               </Box>
             </ListItem>
@@ -918,11 +1132,9 @@ const NoAutorazeMenu = () => {
               t.faq,
               t.blog,
               t.contact,
-            ].map((text) => (
+            ].map(text => (
               <ListItem button key={text}>
-                <ListItemText
-                  primary={<span style={{ color: "blue" }}>{text}</span>}
-                />
+                <ListItemText primary={<Typography color="primary">{text}</Typography>} />
               </ListItem>
             ))}
           </List>
@@ -934,48 +1146,35 @@ const NoAutorazeMenu = () => {
         autoHideDuration={7000}
         onClose={handleAlertClose}
         anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-        sx={{
-          mt: '15vh',
-          zIndex: 1400,
-        }}
-        action={
-          <IconButton
-            size="small"
-            aria-label="close"
-            color="inherit"
-            onClick={handleAlertClose}
-          >
-            <Close fontSize="small" />
-          </IconButton>
-        }
+        sx={{ mt: '10vh', zIndex: 1400 }}
+        action={<IconButton size="small" aria-label="close" color="inherit" onClick={handleAlertClose}><Close fontSize="small"/></IconButton>}
       >
-        <Alert onClose={handleAlertClose} severity="info" sx={{ width: "100%" }}>
+        <Alert onClose={handleAlertClose} severity="info" sx={{ width: '100%' }}>
           {t.registerMessage}
         </Alert>
       </Snackbar>
 
-      <Modal
+      <Dialog
         open={isCreateUserOpen}
         onClose={handleCreateUserClose}
-        aria-labelledby="create-user-modal"
-        aria-describedby="create-user-modal-description"
+        fullWidth
+        maxWidth="xs"
+        PaperProps={{
+          sx: {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            m: 0,
+            p: 0,
+            bgcolor: 'background.paper',
+          }
+        }}
       >
-        <Box
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            width: 400,
-            bgcolor: "background.paper",
-            boxShadow: 24,
-            p: 4,
-            borderRadius: 2,
-          }}
-        >
+        <DialogContent sx={{ p: isSmall ? 1 : 2 }}>
           <CreateUser onClose={handleCreateUserClose} />
-        </Box>
-      </Modal>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };

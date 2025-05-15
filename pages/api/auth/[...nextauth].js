9fe60f118d70dca008987b1d8a665b69759dd650
Ростислав Ермолаев <0988560505r@gmail.com>
2025-05-15 23:53:29 +0300

    ex

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -17,22 +17,16 @@
 
 
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials"; // або Google, GitHub і т.д.
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        username: { label: "Username", type: "text" },
-        password: { label: "Password", type: "password" }
-      },
-      authorize(credentials) {
-        // авторизація
-    console.log(credentials);
-        const user = { id: 1, name: "admin" };
-        return user;
-      }
-    })
-  ]
+  GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID,
+clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+
+  }),
+],
+  secret: process.env.NEXTAUTH_SECRET,
+  
 });

0af7f7ea5178e860e7aa0292bacd2b500ed15ba6
Ростислав Ермолаев <0988560505r@gmail.com>
2025-05-21 19:31:46 +0300

    slfm

--- src/app/components/TestFileUpload.js
+++ src/app/components/TestFileUpload.js
@@ -50,6 +50,69 @@
 
 
 
+// 'use client';
+
+// import React, { useState } from 'react';
+
+// export default function TestFileUpload() {
+//   const [file, setFile] = useState(null);
+//   const [message, setMessage] = useState('');
+//   const [url, setUrl] = useState('');
+
+//   const handleFileChange = (e) => {
+//     setFile(e.target.files[0]);
+//   };
+
+//   const handleUpload = async () => {
+//     if (!file) {
+//       setMessage('Пожалуйста, выберите файл');
+//       return;
+//     }
+
+//     const formData = new FormData();
+//     formData.append('file', file);
+
+//     try {
+//       const res = await fetch('http://localhost:3000/api/v1/upload', {
+//         method: 'POST',
+//         body: formData,
+//       });
+
+//       const data = await res.json();
+
+//       if (res.ok) {
+//         setMessage('✅ Файл загружен!');
+//         setUrl(data.url);
+//       } else {
+//         setMessage(`❌ Ошибка: ${data.error}`);
+//       }
+//     } catch (err) {
+//       console.error(err);
+//       setMessage('❌ Ошибка при загрузке');
+//     }
+//   };
+
+//   return (
+//     <div style={{ maxWidth: 400, margin: 'auto', padding: 20 }}>
+//       <h2>Тест загрузки файла</h2>
+//       <input type="file" onChange={handleFileChange} />
+//       <button onClick={handleUpload} style={{ marginTop: 10 }}>
+//         Загрузить
+//       </button>
+//       {/* {/* {message && <p>{message}</p>} */}
+//       {url && ( */}
+//         {/* // <>
+//         //   <p>📸 Превью:</p>
+//         //   <img src={url} alt="Загруженное фото" width="100%" />
+//         // </> */}
+//       {/* )} */}
+//     </div>
+//   );
+// }
+
+
+
+
 'use client';
 
 import React, { useState } from 'react';
@@ -57,7 +120,6 @@ import React, { useState } from 'react';
 export default function TestFileUpload() {
   const [file, setFile] = useState(null);
   const [message, setMessage] = useState('');
-  const [url, setUrl] = useState('');
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
@@ -82,7 +144,6 @@ export default function TestFileUpload() {
 
       if (res.ok) {
         setMessage('✅ Файл загружен!');
-        setUrl(data.url);
       } else {
         setMessage(`❌ Ошибка: ${data.error}`);
       }
@@ -100,12 +161,6 @@ export default function TestFileUpload() {
         Загрузить
       </button>
       {message && <p>{message}</p>}
-      {url && (
-        <>
-          <p>📸 Превью:</p>
-          <img src={url} alt="Загруженное фото" width="100%" />
-        </>
-      )}
     </div>
   );
 }
